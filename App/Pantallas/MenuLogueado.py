000b78050ae11b9900aeec5ad9915d7adcf5122b
alonsox <alonsox@users.noreply.github.com>
2021-11-06 14:42:07 -0300

    Pequeña enchulada al Menu logueado

--- App/Pantallas/MenuLogueado.py
+++ App/Pantallas/MenuLogueado.py
@@ -5,6 +5,7 @@ from Pantallas.Pantalla import Pantalla
 class MenuLogueado(Pantalla):
     def mostrar(self, usuario: Usuario = None) -> None:
         self.limpiar()
-        print(f'Bienvenido {str(usuario.nombre)}', end='\n\n')
-        self.navMenu().descripcion('MENU LOGUEADO').mostrar(usuario)
+        bienvenide = 'Bienvenida' if usuario.sexo.esFemenino() else 'Bienvenido'
+        mensajeMenu = f"{bienvenide} de vuelta {usuario.nombreCompleto()}"
+        self.navMenu().descripcion(mensajeMenu).mostrar(usuario)
         
